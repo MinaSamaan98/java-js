333a3af249cc25decc4ad103ca271db964289e3a
Marina Samaan <marinasamaan@Marinas-MacBook-Air.local>
2019-02-16 12:39:53 -0800

    add html, script and server files

---
+++ script.js
@@ -0,0 +1,6 @@
+function Calculate (){
+    var billedAmount = document.getElementById("txtBilledAmount").value;
+    var percAmount = document.getElementById("txtPerc").value;
+    var tipAmount = billedAmount * percAmount/100;
+    alert(tipAmount);
+    }
